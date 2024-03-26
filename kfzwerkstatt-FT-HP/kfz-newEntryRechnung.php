8fd130950265afb451f1a729db6b1eb81c32bc93
Huaba01 <138099011+Huaba01@users.noreply.github.com>
2024-03-26 13:17:10 +0100

    Update kfz-newEntryRechnung.php

--- kfzwerkstatt-FT-HP/kfz-newEntryRechnung.php
+++ kfzwerkstatt-FT-HP/kfz-newEntryRechnung.php
@@ -84,6 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <label for="kommentar">Kommentar:</label>
                             <textarea class="form-control" id="kommentar" name="kommentar"></textarea>
                         </div>
+                        <div class="tenor-gif-embed" data-postid="27622065" data-share-method="host" data-aspect-ratio="1.28" data-width="100%"><a href="https://tenor.com/view/tenchi-arick-smoke-cigar-duck-gif-27622065">Tenchi Arick Sticker</a>from <a href="https://tenor.com/search/tenchi-stickers">Tenchi Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
                     </div>
                     <button type="submit" class="btn btn-primary">Rechnung anlegen</button>
                 </div>
